0e6e436c175621a2dc31d405515ce02f5f70bd34
salman0325 <sk7587797@gmail.com>
2025-09-18 21:38:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from CI/CD!');
+  res.send('Hello this is my first CICD through Github Action!');
 });
 
 app.listen(port, () => {
